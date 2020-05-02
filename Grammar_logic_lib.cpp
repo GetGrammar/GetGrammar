c160abad1e0133977f7a746cfb11e3810922a1d0
Mednnn <medn0004@gmail.com>
2020-05-02 21:23:28 +0700

    add search correct words without incorrect, add .gitignore

--- Grammar_logic_lib.cpp
+++ Grammar_logic_lib.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <time.h>
 using namespace std;
 
@@ -8,52 +10,101 @@ void find_dictionary_name(char*& name, int number, int lang)
     if (lang == 0) {
         switch (number) {
         case 1:
-            name = "rus_1.txt";
+            name = "dictionaries/rus_1.txt";
             break;
         case 2:
-            name = "rus_2.txt";
+            name = "dictionaries/rus_2.txt";
             break;
         case 3:
-            name = "rus_3.txt";
+            name = "dictionaries/rus_3.txt";
             break;
         case 4:
-            name = "rus_4.txt";
+            name = "dictionaries/rus_4.txt";
             break;
         case 5:
-            name = "rus_5.txt";
+            name = "dictionaries/rus_5.txt";
             break;
         case 6:
-            name = "rus_6.txt";
+            name = "dictionaries/rus_6.txt";
             break;
         case 7:
-            name = "rus_7.txt";
+            name = "dictionaries/rus_7.txt";
             break;
         }
     }
     if (lang == 1) {
         switch (number) {
         case 1:
-            name = "eng_1.txt";
+            name = "dictionaries/eng_1.txt";
             break;
         case 2:
-            name = "eng_2.txt";
+            name = "dictionaries/eng_2.txt";
             break;
         case 3:
-            name = "eng_3.txt";
+            name = "dictionaries/eng_3.txt";
             break;
         case 4:
-            name = "eng_4.txt";
+            name = "dictionaries/eng_4.txt";
             break;
         case 5:
-            name = "eng_5.txt";
+            name = "dictionaries/eng_5.txt";
             break;
         case 6:
-            name = "eng_6.txt";
+            name = "dictionaries/eng_6.txt";
             break;
         case 7:
-            name = "eng_7.txt";
+            name = "dictionaries/eng_7.txt";
             break;
         }
     }
 }
 
+void get_correct_words_for_first_mode(
+        string& rus_correct, string& eng_correct, int dict_number, int sector)
+{
+    srand(time(0));
+    int num_rand;
+    char* name_rus;
+    char* name_eng;
+    find_dictionary_name(name_rus, dict_number, 0);
+    find_dictionary_name(name_eng, dict_number, 1);
+    ifstream rus, eng;
+    string a, rus_correct_1, eng_correct_1;
+    rus.open(name_rus);
+    eng.open(name_eng);
+    if (sector == 9)
+        num_rand = 630 + rand() % 66;
+    else
+        num_rand = 70 * sector + rand() % 70;
+    for (int i = 0; i < num_rand; ++i) {
+        getline(rus, a);
+        getline(eng, a);
+    }
+    getline(rus, rus_correct_1);
+    getline(eng, eng_correct_1);
+    rus_correct = rus_correct_1;
+    eng_correct = eng_correct_1;
+}
+
+void correct_init(string a[10], string b[10], string c[30])
+{
+    int dict_number = rand() % 7 + 1;
+    for (int i = 0; i < 10; ++i) {
+        get_correct_words_for_first_mode(a[i], b[i], dict_number, i);
+    }
+    int incorrect_dict_number = dict_number;
+    while (dict_number = incorrect_dict_number)
+        incorrect_dict_number = rand() % 7 + 1;
+}
+
+int main()
+{
+    srand(time(0));
+    string rus_correct[10];
+    string eng_correct[10];
+    string incorrect[30];
+    correct_init(rus_correct, eng_correct, incorrect);
+    for (int i = 0; i < 10; ++i) {
+        cout << rus_correct[i] << " " << eng_correct[i] << endl;
+    }
+}
