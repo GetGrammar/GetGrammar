26432c7e2eb3edc3d1b5fb8c916c8e310d12ec5f
Kurzin-Andrej <aymr.fmy@mail.ru>
2020-06-06 16:27:36 +0700

    Add test is_repeat

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,37 @@
 #include "Grammar_logic_lib.h"
 #include "getgrammar_main.h"
 
+CTEST(is_repeat, REPEAT_TRUE)
+{
+    int mas[4] = {1, 2, 3, 4};
+    bool result = is_repeat(mas, 4);
+
+    ASSERT_TRUE(result);
+}
+
+CTEST(is_repeat, REPEAT_FALSE)
+{
+    int mas[4] = {1, 2, 4, 4};
+    bool result = is_repeat(mas, 4);
+
+    ASSERT_FALSE(result);
+}
+
+CTEST(is_repeat, REPEAT_CURRENT_TRUE)
+{
+    int mas[4] = {1, 2, 3, 4};
+    bool result = is_repeat(mas, 4, 5);
+
+    ASSERT_TRUE(result);
+}
+
+CTEST(is_repeat, REPEAT_CURRENT_FALSE)
+{
+    int mas[4] = {1, 2, 3, 4};
+    bool result = is_repeat(mas, 4, 4);
+
+    ASSERT_FALSE(result);
+}
 
 int main(int argc, const char** argv)
 {
