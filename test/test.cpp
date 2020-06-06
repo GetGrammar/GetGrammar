d1925c57513f0d402ea214911f313fb9b33fb157
Kurzin-Andrej <aymr.fmy@mail.ru>
2020-06-06 16:20:25 +0700

    Add test

---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+#define CTEST_MAIN
+
+#include <ctest.h>
+#include <stdio.h>
+#include "Grammar_logic_lib.h"
+#include "getgrammar_main.h"
+
+
+int main(int argc, const char** argv)
+{
+    return ctest_main(argc, argv);
+}
